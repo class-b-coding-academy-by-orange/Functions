406faf495844f1dd3dd6271d129aa1c268a9cf7d
MohammadJozaa7 <m.jozaa@yahoo.com>
2019-07-09 08:22:12 +0300

    9 questions

---
+++ function.js
@@ -0,0 +1,173 @@
+/*
+1
+Write a function named tellFortune that:
+takes 4 arguments: number of children,
+partner's name, geographic location, job title.
+outputs your fortune to the screen like so:
+"You will be a X in Y, and married to Z with N kids."
+
+Ex: tellFortune('ball player', 'spain', 'Shaq', 3);
+=> "You will be a ball player in spain, and married to Shaq with 3 kids."
+*/
+
+
+/*
+2
+Write a function named calculateDogAge that:
+takes 1 argument: your puppy's age.
+calculates your dog's age based on the conversion rate of 1 human year to 7 dog years.
+outputs the result to the screen like so: "Your doggie is NN years old in dog years!"
+
+Ex: calculateDogAge(1);
+=> "Your doggie is 7 years old in dog years!"
+*/
+
+
+/*
+3
+Write a function named calculateSupply that:
+takes 2 arguments: age, amount per day.
+calculates the amount consumed for rest of the life (based on a constant max age 100).
+outputs the result to the screen like so:
+"You will need NN to last you until the ripe old age of X"
+
+Ex: calculateSupply(30, 3);
+=> 'You will need 76650 cups of tea to last you until the ripe old age of 100;
+*/
+
+
+/*
+4
+Write a function called greet that:
+takes 1 argument: name.
+and it will return hello + name
+
+Ex: greet("Jouza")
+=> "Hello Jouza"
+*/
+
+
+/*
+5
+what is the error:
+function double(cat) {
+  return 2 * x;
+}
+
+function double(7) {
+  return 2 * 7;
+}
+
+function double('7') {
+  return 2 * 'x';
+}
+*/
+
+
+
+/*
+6
+fix these functions:
+func double1(x {
+  return 2 * x ;
+}
+
+functiondouble2 x)
+return 2 * x;
+}
+
+function (x) double3 {
+  return 2 * x;
+
+*/
+
+
+/*
+7
+Write a function called cube that:
+accept 1 parameter and calculate the cube of this number
+
+Ex: cube(4)
+=> 64
+*/
+
+
+/*
+Write a function called multiply that:
+accept 2 parameters and calculate the multiply of these 2 numbers
+
+Ex: multiply(3,4)
+=> 12
+
+*/
+
+
+/*
+Write a function called canIGetADrivingLicense that:
+accept 1 parameter represent the age
+and if the age greater than or equal to 20 return "yes you can"
+otherwise return "please come back after X years to get one"
+
+Ex: canIGetADrivingLicense(21)
+=> "yes you can"
+
+Ex: canIGetADrivingLicense(17)
+=> "please come back after 3 years to get one"
+
+*/
+
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
+
+/*
+
+
+*/
